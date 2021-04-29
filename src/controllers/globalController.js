9e541c20d2f0c3f683526dd40cb76cc3814fe9d8
“jungkeunmo“ <abc59684495@gmail.com>
2021-04-29 23:50:25 +0900

    crud

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,8 +1,12 @@
 const homeController = (req, res) => {
+    //db에서 메모를 가져온다.
     res.render("screens/home");
 };
 
 const detailController = (req, res) => {
+    //사용자가 선택한 메모의 id값을 가져온뒤,
+    //db에서 해당 아이디와 일치하는 데이터를 조회한다.
+    
     res.render("screens/detail");
 };
 
